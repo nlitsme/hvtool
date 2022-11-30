e8f0c8dba61cf923e60ee5fbb9a31e628fb710b0
itsme <itsme@xs4all.nl>
2022-11-30 12:50:26 +0100

    finder for itslib

--- cmake/Finditslib.cmake
+++ cmake/Finditslib.cmake
@@ -1,4 +1,21 @@
-add_library(itslib STATIC itslib/src/vectorutils.cpp itslib/src/stringutils.cpp itslib/src/debug.cpp)
-target_include_directories(itslib PUBLIC itslib/include)
+find_path(ITSLIB_PATH NAMES include/itslib/stringutils.h PATHS symlinks/itslib)
+if(ITSLIB_PATH STREQUAL "ITSLIB_PATH-NOTFOUND")
+    include(FetchContent)
+    FetchContent_Populate(itslib
+        GIT_REPOSITORY https://github.com/nlitsme/legacy-itslib-library)
+    set(ITSLIB_PATH  ${itslib_SOURCE_DIR})
+else()
+    set(itslib_BINARY_DIR ${CMAKE_BINARY_DIR}/itslib-build)
+endif()
+set(ITSLIB_INCLUDE_DIR ${ITSLIB_PATH}/include/itslib)
+
+list(APPEND ITSLIBSRC debug.cpp stringutils.cpp utfcvutils.cpp vectorutils.cpp FileFunctions.cpp)
+list(TRANSFORM ITSLIBSRC PREPEND ${ITSLIB_PATH}/src/)
+add_library(itslib STATIC ${ITSLIBSRC})
+target_include_directories(itslib PUBLIC ${ITSLIB_INCLUDE_DIR})
 target_compile_definitions(itslib PUBLIC _NO_RAPI _NO_WINDOWS)
-target_include_directories(itslib PUBLIC  ${Boost_INCLUDE_DIRS})
+if(NOT WIN32)
+    target_compile_definitions(itslib PUBLIC _UNIX)
+endif()
+
+
