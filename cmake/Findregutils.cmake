4e611098e0140efb004bc9a2fbf9a6dbf1026dc1
itsme <itsme@xs4all.nl>
2023-08-22 20:29:44 +0200

    the hvtool package now also declares the reglib library

--- cmake/Findregutils.cmake
+++
@@ -1,3 +0,0 @@
-add_library(regutils STATIC registryutils/regfileparser.cpp registryutils/regvalue.cpp)
-target_include_directories(regutils PUBLIC registryutils)
-target_link_libraries(regutils PRIVATE itslib)
