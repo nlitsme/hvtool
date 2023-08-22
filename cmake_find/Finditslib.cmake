e756cb5168ead232f0e3cf0e4403c174bf8d4c37
itsme <itsme@xs4all.nl>
2023-08-22 20:29:44 +0200

    moved to cmake_find. and updated, itslib now is a cmake library.

---
+++ cmake_find/Finditslib.cmake
@@ -0,0 +1,15 @@
+if (TARGET itslib)
+    return()
+endif()
+find_path(ITSLIB_PATH NAMES include/itslib/stringutils.h PATHS symlinks/itslib)
+if(ITSLIB_PATH STREQUAL "ITSLIB_PATH-NOTFOUND")
+    include(FetchContent)
+    FetchContent_Populate(itslib
+        GIT_REPOSITORY https://github.com/nlitsme/legacy-itslib-library)
+    set(ITSLIB_PATH  ${itslib_SOURCE_DIR})
+else()
+    set(itslib_BINARY_DIR ${CMAKE_BINARY_DIR}/itslib-build)
+endif()
+
+add_subdirectory(${ITSLIB_PATH})
+
